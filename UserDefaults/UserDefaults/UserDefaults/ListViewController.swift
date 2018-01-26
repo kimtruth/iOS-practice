d196a3272b181957ce8974c52326614903fafd19
j28150 <j28150@naver.com>
2018-01-26 23:25:29 +0900

    데이터 저장 및 불러오기

--- UserDefaults/UserDefaults/UserDefaults/ListViewController.swift
+++ UserDefaults/UserDefaults/UserDefaults/ListViewController.swift
@@ -9,18 +9,35 @@
 import UIKit
 
 class ListViewController: UITableViewController {
+  
   @IBOutlet weak var name: UILabel!
   @IBOutlet weak var gender: UISegmentedControl!
   @IBOutlet weak var married: UISwitch!
   
   @IBAction func changeGender(_ sender: UISegmentedControl) {
+    let value = sender.selectedSegmentIndex
+    
+    let plist = UserDefaults.standard
+    plist.set(value, forKey: "gender")
+    plist.synchronize()
   }
   
   @IBAction func changeMarried(_ sender: UISwitch) {
+    let value = sender.isOn
+    
+    let plist = UserDefaults.standard
+    plist.set(value, forKey: "married")
+    plist.synchronize()
   }
   
   override func viewDidLoad() {
     super.viewDidLoad()
+    
+    let plist = UserDefaults.standard
+    
+    self.name.text = plist.string(forKey: "name")
+    self.gender.selectedSegmentIndex = plist.integer(forKey: "gender")
+    self.married.isOn = plist.bool(forKey: "married")
   }
   
   override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
@@ -36,6 +53,14 @@ class ListViewController: UITableViewController {
       /// 버튼 및 액션 추가
       alert.addAction(UIAlertAction(title: "OK", style: .default) { _ in
         /// 사용자가 OK 버튼을 누르면 입력 필드에 입력된 값을 저장한다.
+        let value = alert.textFields?[0].text
+        
+        let plist = UserDefaults.standard
+        plist.set(value, forKey: "name")
+        plist.synchronize()
+        
+        // 수정된 값을 이름 레이블에도 적용한다.
+        self.name.text = value
       })
       /// 알림창을 띄운다.
       self.present(alert, animated: true, completion: nil)
