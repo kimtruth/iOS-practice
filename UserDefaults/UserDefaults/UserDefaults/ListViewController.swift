4785b5c82475e0a8cddee68e20edd8791346d632
j28150 <j28150@naver.com>
2018-01-27 23:51:58 +0900

    탭 제스처를 사용하여 액션 메소드 처리

--- UserDefaults/UserDefaults/UserDefaults/ListViewController.swift
+++ UserDefaults/UserDefaults/UserDefaults/ListViewController.swift
@@ -14,6 +14,31 @@ class ListViewController: UITableViewController {
   @IBOutlet weak var gender: UISegmentedControl!
   @IBOutlet weak var married: UISwitch!
   
+  @IBAction func edit(_ sender: UITapGestureRecognizer) {
+    /// 입력이 가능한 알림창을 띄워 이름을 수정할 수 있도록 한다.
+    let alert = UIAlertController(title: nil, message: "이름을 입력하세요", preferredStyle: .alert)
+    
+    /// 입력 필드 추가
+    alert.addTextField() {
+      /// name 레이블의 텍스트를 입력폼에 기본값으로 넣어준다.
+      $0.text = self.name.text
+    }
+    /// 버튼 및 액션 추가
+    alert.addAction(UIAlertAction(title: "OK", style: .default) { _ in
+      /// 사용자가 OK 버튼을 누르면 입력 필드에 입력된 값을 저장한다.
+      let value = alert.textFields?[0].text
+      
+      let plist = UserDefaults.standard
+      plist.set(value, forKey: "name")
+      plist.synchronize()
+      
+      // 수정된 값을 이름 레이블에도 적용한다.
+      self.name.text = value
+    })
+    /// 알림창을 띄운다.
+    self.present(alert, animated: false, completion: nil)
+  }
+  
   @IBAction func changeGender(_ sender: UISegmentedControl) {
     let value = sender.selectedSegmentIndex
     
@@ -38,33 +63,6 @@ class ListViewController: UITableViewController {
     self.name.text = plist.string(forKey: "name")
     self.gender.selectedSegmentIndex = plist.integer(forKey: "gender")
     self.married.isOn = plist.bool(forKey: "married")
-  }
-  
-  override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-    if indexPath.row == 0 {
-      /// 입력이 가능한 알림창을 띄워 이름을 수정할 수 있도록 한다.
-      let alert = UIAlertController(title: nil, message: "이름을 입력하세요", preferredStyle: .alert)
-      
-      /// 입력 필드 추가
-      alert.addTextField() {
-        /// name 레이블의 텍스트를 입력폼에 기본값으로 넣어준다.
-        $0.text = self.name.text
-      }
-      /// 버튼 및 액션 추가
-      alert.addAction(UIAlertAction(title: "OK", style: .default) { _ in
-        /// 사용자가 OK 버튼을 누르면 입력 필드에 입력된 값을 저장한다.
-        let value = alert.textFields?[0].text
-        
-        let plist = UserDefaults.standard
-        plist.set(value, forKey: "name")
-        plist.synchronize()
-        
-        // 수정된 값을 이름 레이블에도 적용한다.
-        self.name.text = value
-      })
-      /// 알림창을 띄운다.
-      self.present(alert, animated: true, completion: nil)
-    }
   }
   
 }
